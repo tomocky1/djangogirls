c5a3d3ae2d0d293f8ebed596d02be0f3f18b85df
tomocky1 <siokobu8400@gmail.com>
2023-06-23 08:26:16 +0900

    ある程度更新

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Post
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+class PostTestCase(TestCase):
+    def test_posts_get(self):
+        Post.objects.create(author=get_user_model(),title="タイトル1",text="本文1")
+        post = Post.objects.get(title="タイトル1")
+        self.assertEqual(post.title, "タイトル1")
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iweygj5jkv41#(bo3tiqi$9o0)nc)=la2gn7t($v$(1)7u-9zq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
